82790ede6682b3e176b48c1a4896ef7bc31f7b17
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2023-09-08 00:43:47 +0000

    Desarrollo Analizador lexico

---
+++ Analizador.py
@@ -0,0 +1,127 @@
+import ply.lex as lex
+
+# Palabras reservadas
+reserved = {
+    'Proceso': 'Inicio_Proceso',
+    'FinProceso': 'Fin_Proceso',
+    'Si': 'Condiciones_Si',
+    'Sino': 'Condiciones_Sino',
+    'FinSi': 'Condiciones_FinSi',
+    'mientras': 'Bucle',
+    'Imprimir': 'Impresor',
+    'entero': 'Dato_Int',
+    'largo': 'Dato_Long',
+    'flotante': 'Dato_Flotante',
+    'caracter': 'Dato_Caracter',
+    'booleano': 'Dato_Booleano',
+}
+
+# Lista de tokens
+tokens = [
+    'Identificador', 'Num_Entero', 'Num_Decimales', 'Op_Suma', 'Op_Resta',
+    'Op_Multiplicacion', 'Op_Division', 'Mayor_Q', 'Menor_Q', 'Igualdad',
+    'Comparacion', 'Mayor_Igual', 'Menor_Igual', 'Incremento',
+    'Comparacion_Igualdad', 'Op_Logico', 'Sum_Asignacion', 'Decremento',
+    'Cadena_Texto', 'Comentario', 'Inicio_Parentesis', 'Fin_Parentesis',
+    'Punto_Coma', 'Parentesis_Abrir', 'Parentesis_Cerrar', 'Llave_Abrir',
+    'Llave_Cerrar'
+] + list(reserved.values())
+
+# Expresiones regulares para los tokens
+t_Op_Suma = r'\+'
+t_Op_Resta = r'-'
+t_Op_Multiplicacion = r'\*'
+t_Op_Division = r'%|/'
+t_Mayor_Q = r'>'
+t_Menor_Q = r'<'
+t_Igualdad = r'='
+t_Comparacion = r'=='
+t_Mayor_Igual = r'>='
+t_Menor_Igual = r'<='
+t_Incremento = r'\+\+'
+t_Comparacion_Igualdad = r'!='
+t_Op_Logico = r'OR'
+t_Sum_Asignacion = r'\+='
+t_Decremento = r'--'
+t_Parentesis_Abrir = r'\('
+t_Parentesis_Cerrar = r'\)'
+t_Llave_Abrir = r'\{'
+t_Llave_Cerrar = r'\}'
+
+
+# Regla para manejar identificadores
+def t_Identificador(t):
+  r'[_a-zA-Z][_a-zA-Z0-9]*'
+  t.type = reserved.get(t.value,
+                        'Identificador')  # Verificar palabras reservadas
+  return t
+
+
+def t_Num_Decimales(t):
+  r'\d+\.\d+'
+  t.value = float(t.value)  # guardamos el valor del lexema
+  #print("se reconocio el numero")
+  return t
+
+
+def t_Num_Entero(t):
+  r'\d+'
+  t.value = int(t.value)  # guardamos el valor del lexema
+  #print("se reconocio el numero")
+  return t
+
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+
+
+# Error handling rule
+def t_error(t):
+  print("Illegal character '%s'" % t.value[0])
+  t.lexer.skip(1)
+
+
+# Regla para manejar comentarios
+def t_Comentario(t):
+  r'\/\/.*'
+  return t  # Los comentarios se ignorarán
+
+
+# Regla para manejar cadenas de texto
+def t_Cadena_Texto(t):
+  r'"([^"\\]|\\.)*"'
+  t.value = str(t.value)
+  return t
+
+
+# Build the lexer
+lexer = lex.lex()
+
+# Prueba del analizador léxico
+data = '''Proceso
+    entero a = 5
+    flotante b = 3.14
+    caracter c = "X"
+    booleano d = verdadero
+    cadena e = "Hola, mundo!"
+    mientras (a > 0) {
+        Si (a % 2 == 0) {
+            Imprimir "a es par"
+        } Sino {
+            Imprimir "a es impar"
+        }
+        a -= 1
+    }
+FinProceso
+// Esto es un comentario de línea
+'''
+
+lexer.input(data)
+
+for tok in lexer:
+  print(tok.type, tok.value)
